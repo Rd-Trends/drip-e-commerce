af026b41118d5e378241ebfdcf7c1304ed3d2f99
Daniel Ikoyo <dannirolands@gmail.com>
2026-05-31 20:29:25 +0100

    tiktok pixel

--- public/scripts/tiktok-pixel.js
+++ public/scripts/tiktok-pixel.js
@@ -3,57 +3,10 @@
 const script = document.getElementById('tiktok-pixel')
 const PIXEL_ID = script ? script.getAttribute('data-pixel-id') : null
 
-function initializeTikTokPixel(w, d, t) {
-  w.TiktokAnalyticsObject = t
-  var ttq = (w[t] = w[t] || [])
-  ttq.methods = [
-    'page',
-    'track',
-    'identify',
-    'instances',
-    'debug',
-    'on',
-    'off',
-    'once',
-    'ready',
-    'alias',
-    'group',
-    'enableCookie',
-    'disableCookie',
-    'holdConsent',
-    'revokeConsent',
-    'grantConsent',
-  ]
-  ttq.setAndDefer = function (t, e) {
-    t[e] = function () {
-      t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
-    }
-  }
-  for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i])
-  ttq.instance = function (t) {
-    for (var e = (w[t] = w[t] || []), n = 0; n < ttq.methods.length; n++)
-      ttq.setAndDefer(e, ttq.methods[n])
-    return e
-  }
-  ttq.load = function (e, n) {
-    var i = 'https://analytics.tiktok.com/i18n/pixel/events.js'
-    ttq._i = ttq._i || {}
-    ttq._i[e] = []
-    ttq._i[e]._u = i
-    ttq._t = ttq._t || {}
-    ttq._t[e] = +new Date()
-    ttq._o = ttq._o || {}
-    ttq._o[e] = n || {}
-    var o = document.createElement('script')
-    o.type = 'text/javascript'
-    o.async = true
-    o.src = i + '?sdkid=' + e + '&lib=' + t
-    var a = document.getElementsByTagName('script')[0]
-    a.parentNode.insertBefore(o, a)
-  }
-}
-
 if (PIXEL_ID) {
-  initializeTikTokPixel(window, document, 'ttq')
-  window.ttq.load(PIXEL_ID)
+  !function (w, d, t) {
+    w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+    ttq.load(PIXEL_ID);
+    ttq.page();
+  }(window, document, 'ttq');
 }
--- src/providers/analytics-pixel.tsx
+++ src/providers/analytics-pixel.tsx
@@ -4,7 +4,7 @@ import * as fbPixel from '@/lib/facebook-pixel'
 import * as ttPixel from '@/lib/tiktok-pixel'
 import { usePathname } from 'next/navigation'
 import Script from 'next/script'
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
 
 type AnalyticsPixelContextType = {
   fbLoaded: boolean
@@ -21,6 +21,9 @@ const AnalyticsPixelContext = createContext<AnalyticsPixelContextType>({
 const AnalyticsPixelProvider = ({ children }: { children: React.ReactNode }) => {
   const [fbLoaded, setFbLoaded] = useState(false)
   const [ttLoaded, setTtLoaded] = useState(false)
+  // The TikTok init script already calls ttq.page() on first load.
+  // Skip that first call from React to avoid double-counting.
+  const ttInitialPageViewFired = useRef(false)
   const pathname = usePathname()
 
   useEffect(() => {
@@ -28,7 +31,12 @@ const AnalyticsPixelProvider = ({ children }: { children: React.ReactNode }) => 
   }, [pathname, fbLoaded])
 
   useEffect(() => {
-    if (ttLoaded) ttPixel.pageview()
+    if (!ttLoaded) return
+    if (!ttInitialPageViewFired.current) {
+      ttInitialPageViewFired.current = true
+      return
+    }
+    ttPixel.pageview()
   }, [pathname, ttLoaded])
 
   return (
@@ -57,16 +65,7 @@ const AnalyticsPixelProvider = ({ children }: { children: React.ReactNode }) => 
           src="/scripts/tiktok-pixel.js"
           strategy="afterInteractive"
           onLoad={() => setTtLoaded(true)}
-          dangerouslySetInnerHTML={{
-            __html: `!function (w, d, t) {
-            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
-          var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
-          ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-
-
-            ttq.load('${ttPixel.TIKTOK_PIXEL_ID}');
-          }(window, document, 'ttq');`,
-          }}
+          data-pixel-id={ttPixel.TIKTOK_PIXEL_ID}
         />
       )}
     </AnalyticsPixelContext.Provider>
